5fea9e682da4c55336f0a84cb94cef8b6e995937
liz <lizaraju@gmail.com>
2019-08-13 11:46:38 -0400

    Final commit by Indian team

---
+++ E2EProject/src/test/java/Academy/demo.java
@@ -0,0 +1,22 @@
+package Academy;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+
+
+public class demo {
+
+@Test(dataProvider="getData")
+	
+	public void basePageNavigation(String Username,String Password,String text) throws IOException
+	{
+
+		
+		
+		System.out.println("Mummy was here");
+		System.out.println("dady was here");
+
+}
+}
